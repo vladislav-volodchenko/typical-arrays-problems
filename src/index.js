5044a624beb2a6673dc854dfdb95b6e6b11e92b7
vladislav-volodchenko <70842775+vladislav-volodchenko@users.noreply.github.com>
2020-09-30 15:43:53 +0300

    init: start typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array && array.length > 0) {
+  	return Math.min.apply(null, array);
+  } else {
+  	return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array && array.length > 0) {
+  	return Math.max.apply(null, array);
+  } else {
+  	return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array && array.length > 0) {
+  	return array.reduce((a, b) => (a + b)) / array.length;
+  } else {
+  	return 0;
+  }
 }
